addae96427f60dfa7577232be5b31043d202e1ea
SidN <nsidhartha@gmail.com>
2018-11-25 15:36:25 +0530

    commit of working version

---
+++ src/main/java/springRest/HelloWorldApplication.java
@@ -0,0 +1,9 @@
+package springRest;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class HelloWorldApplication extends ResourceConfig {
+    public HelloWorldApplication() {
+        // Define the package which contains the service classes.
+        packages("springRest");
+    }
+}
